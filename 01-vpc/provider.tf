96335abd19e6b47ba31c86c0b96298c4d0d91ec4
naresh <Naresh@gmail.com>
2025-12-09 13:19:43 +0530

    removed region from actions

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
  
     }
 backend "s3" {
-   bucket = "aws-infra-terraform-statefile-bucket-001"
+   bucket = "aws-infra-terraform-statefile-bucket-mumbai-002"
    key    = "vpc/qa/terraform.tfstate"
    encrypt = true
    use_lockfile = true
