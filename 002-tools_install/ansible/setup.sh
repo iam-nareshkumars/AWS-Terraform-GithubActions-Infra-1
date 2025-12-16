add5698f32dc0a53c94f0977a0fe802d0c0fe638
naresh <Naresh@gmail.com>
2025-12-16 21:53:40 +0530

    ansible integration testing

--- 002-tools_install/ansible/setup.sh
+++ 002-tools_install/ansible/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-ansible-playbook -i "002-tools_install/ansible/inventory/aws_ec2.yml"  -e toolname=vault  002-tools_install/ansible/playbooks/main.yml
+ansible-playbook -i "002-tools_install/ansible/inventory/aws_ec2.yml"  -e toolname=vault  002-tools_install/ansible/playbooks/main.yml  --limit tag_Tool_vault
