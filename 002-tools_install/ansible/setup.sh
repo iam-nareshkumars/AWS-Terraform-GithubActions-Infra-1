23f74a6b36891b9509b93f6ed05fbe344eecee90
naresh <Naresh@gmail.com>
2025-12-16 20:13:42 +0530

    ansible integration testing

--- 002-tools_install/ansible/setup.sh
+++ 002-tools_install/ansible/setup.sh
@@ -3,4 +3,4 @@ set -e
 
 TOOL=$1
 
-ansible-playbook -e toolname=$1 002-tools_install/ansible/main.yml
+ansible-playbook -i "002-tools_install\ansible\inventory\aws_ec2.yml"  -e toolname=$1 002-tools_install/ansible/main.yml
