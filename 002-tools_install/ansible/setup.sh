d55de66343f223533a17d0dc69da920f7453c552
naresh <Naresh@gmail.com>
2025-12-17 12:28:46 +0530

    ansible local

--- 002-tools_install/ansible/setup.sh
+++ 002-tools_install/ansible/setup.sh
@@ -3,4 +3,4 @@ set -e
 
 # ansible-playbook -i "002-tools_install/ansible/inventory/aws_ec2.yml"  -e toolname=vault  002-tools_install/ansible/playbooks/main.yml  --limit tag_Tool_vault
 
-ansible-playbook -i "002-tools_install/ansible/inventory/aws_ec2.yml"   /home/ec2-user/AWS-Terraform-GithubActions-Infra-1/002-tools_install/ansible/playbooks/main.yml
+ansible-playbook -i "002-tools_install/ansible/inventory/aws_ec2.yml"  -e toolname=vault /home/ec2-user/AWS-Terraform-GithubActions-Infra-1/002-tools_install/ansible/playbooks/main.yml
