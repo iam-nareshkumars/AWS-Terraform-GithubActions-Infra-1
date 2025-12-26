566e87df040c1b34c0ca7656b864a13b6b7ca043
naresh <Naresh@gmail.com>
2025-12-26 21:06:09 +0530

    tool config using environments

--- 002-tools_install/terraform/modules/iam.tf
+++ 002-tools_install/terraform/modules/iam.tf
@@ -1,49 +1,49 @@
-# resource "aws_iam_role" "main" {
-#   name = "${var.Name}-IAM-role"
+resource "aws_iam_role" "main" {
+  name = "${var.Name}-IAM-role"
 
-#   # Terraform's "jsonencode" function converts a
-#   # Terraform expression result to valid JSON syntax.
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Sid    = ""
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 
-#   tags = {
-#     tag-key = "${var.Name}-IAM-role"
-#   }
-# }
+  tags = {
+    tag-key = "${var.Name}-IAM-role"
+  }
+}
 
 
-# resource "aws_iam_role_policy" "main" {
-#   name = "${var.Name}-IAM-role-policy"
-#   role = aws_iam_role.main.id
+resource "aws_iam_role_policy" "main" {
+  name = "${var.Name}-IAM-role-policy"
+  role = aws_iam_role.main.id
 
-#   # Terraform's "jsonencode" function converts a
-#   # Terraform expression result to valid JSON syntax.
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = [
-#           "ec2:DescribeInstances",
-#           "ec2:DescribeAvailabilityZones"
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:DescribeAvailabilityZones"
 
-#         ]
-#         Effect   = "Allow"
-#         Resource = "*"
-#       },
-#     ]
-#   })
-# }
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
 
 # resource "aws_iam_instance_profile" "robo" {
 #   name = "${var.Name}-iam_instance_profile"
