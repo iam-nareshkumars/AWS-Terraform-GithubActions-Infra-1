bf87200d92c72f48ded156fc0c0860bf13480481
naresh <Naresh@gmail.com>
2025-12-26 20:14:24 +0530

    ansible integration testing

---
+++ 01-vpc/prod/backend.tf
@@ -0,0 +1,5 @@
+bucket         = "aws-infra-terraform-statefile-bucket-001"
+key            = "sg/prod/terraform.tfstate"
+region         = "us-east-1"
+encrypt        = true
+use_lockfile   = true
---
+++ 01-vpc/qa/backend.tf
@@ -0,0 +1,5 @@
+bucket         = "aws-infra-terraform-statefile-bucket-001"
+key            = "sg/qa/terraform.tfstate"
+region         = "us-east-1"
+encrypt        = true
+use_lockfile   = true
